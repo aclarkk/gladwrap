65d46b7221a264a2c3d69da081843e3530ba9c70
Alan Clark <a.clark5094@gmail.com>
2015-06-16 16:35:02 -0400

    write files to subdir

--- lib/gladwrap.rb
+++ lib/gladwrap.rb
@@ -5,6 +5,8 @@ require "slop"
 
 class Gladwrap
 
+  output_directory = './wraps/'
+
   opts = Slop.parse do |o|
     o.string  '--device', '-d', 'Which device to use (iphone6_black, iphone6_white, more coming soon)', default: 'iphone6_black'
     o.on '--help', '-h' do
@@ -21,14 +23,26 @@ class Gladwrap
     bgURL = "https://s3.amazonaws.com/gladwrapp-assets/#{device}.png"
     bg = Magick::Image.read(bgURL).first
     files = Dir.glob"*.png"
-    puts "We bout to wrap bout #{files.count} files."
+
+    if files.count < 1
+      puts "I don't see any files. What the heck." 
+    elsif
+      puts "We bout to wrap bout #{files.count} files."
+    end
+
+    unless File.exist?(output_directory)
+      puts "creating #{output_directory}"
+      Dir.mkdir(output_directory)
+    end
 
     files.each_with_index do |f, index| 
       overlay = Magick::Image.read("#{f}").first
       bg.composite!(overlay, 51, 217, Magick::OverCompositeOp)
-      bg.write("#{index}.png")
-      puts "writing #{index}.png"
+      bg.write("./#{output_directory}/#{index}.png")
+      puts "writing #{index}.png to #{output_directory}"
     end
+
+    puts "We all done here."
 
   rescue Slop::Error => e
     puts e.message
